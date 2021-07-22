e11b2b0a6cd93c0f6999d25f102ae75ef8a4d389
chucoyos <facturasmanzanillo@hotmail.com>
2021-07-22 14:31:32 -0500

    add nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'pages/home'
   resources :services do
     resource :completed
   end
-  root to: 'services#index'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
